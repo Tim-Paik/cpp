4957706c98f07f37198ef0eb61a8080d296c16ef
Tim-Paik <timpaik@163.com>
2021-06-05 21:58:59 +0800

    2021-06-05

---
+++ oj/1.3/04.cpp
@@ -0,0 +1,9 @@
+//04:带余除法
+#include <iostream>
+using namespace std;
+int main() {
+	int a,b;
+	cin >> a >> b;
+	cout << a/b << " " << a%b << endl;
+	return 0;
+}
---
+++ oj/1.3/05.cpp
@@ -0,0 +1,8 @@
+//05:计算分数的浮点数值
+#include <cstdio>
+int main() {
+	int a,b;
+	scanf("%d %d",&a,&b);
+	printf("%.9f",a*1.0/b);
+	return 0;
+}
